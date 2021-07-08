a73cd985f63bc0f4d5a5f3674e1920686032b332
hank <guanhanchen6@gmail.com>
2021-07-08 20:45:26 +0800

    add Azure module

--- app.py
+++ app.py
@@ -10,10 +10,23 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+
+
+from linebot.models import *
+import json
+import tempfile, os
+
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from msrest.authentication import CognitiveServicesCredentials
+
+
+
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('OumKW26vOPeAJG8TDdEM0+ZZDzLd6TLku8dvR2ehYZY/DKqocxS60EwQSCoXAXTfmZY+ggmzNviPHAC1IDZOxgJx0lxkhz9PMxf/WsnXqrS5CDphuNizzVg/9Y1wus5eYL1rPVxX+1QHJELOSwwQYQdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('d99f3d4ad026ffb7552e53b2bd010ed5')
+
 
 
 @app.route("/callback", methods=['POST'])
@@ -35,12 +48,53 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
+
+#接收圖片
+@handler.add(MessageEvent, message=(ImageMessage, TextMessage))
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    if isinstance(event.message, ImageMessage):
+        ext = 'jpg'
+        message_content = line_bot_api.get_message_content(event.message.id)
+        with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-', delete=False) as tf:
+            for chunk in message_content.iter_content():
+                tf.write(chunk)
+            tempfile_path = tf.name
 
+        dist_path = tempfile_path + '.' + ext
+        dist_name = os.path.basename(dist_path)
+        os.rename(tempfile_path, dist_path)
+        try:
+  
+            path = os.path.join('static', 'tmp', dist_name)
+            print(path) 
 
+        except:
+            line_bot_api.reply_message(
+                event.reply_token, [
+                    TextSendMessage(text=' yoyo'),
+                    TextSendMessage(text='請傳一張圖片給我')
+                ])
+
+#圖片轉敘述api
+        # Set API key.
+        subscription_key = 'key'
+
+        # Set endpoint.
+        endpoint = 'endpoint'
+
+        # Call API
+        computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+
+        # 指定圖檔
+        local_image_path = os.getcwd() + '/static/tmp/{}'.format(path.split('/')[-1])
+
+        # 讀取圖片
+        local_image = open(local_image_path, "rb")
+
+        description_results = computervision_client.describe_image_in_stream(local_image)
+        
+#回傳圖片描述
+        for caption in description_results.captions:
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(caption.text))
 if __name__ == "__main__":
     app.run()
